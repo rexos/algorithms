70cc136a9c32bb906b4364cfd2b4403682b0fc48
rexos <alex.pellegrini@live.com>
2013-04-29 19:14:35 +0200

    added expression

--- componente.cpp
+++ componente.cpp
@@ -17,7 +17,7 @@ struct node{
 vector<node> graph;
 vector<node> trasp;
 
-int count = 0;
+int counter = 0;
 
 void dfs(int n){
   if(!graph[n].found){
@@ -27,6 +27,7 @@ void dfs(int n){
       int tmp = graph[n].adj[i];
       dfs(tmp);
     }
+    //    graph[n].found = false;
   }
 }
 
@@ -34,11 +35,13 @@ void dfstrasp(int n){
   if(!trasp[n].found){
     trasp[n].found = true;
     if (trasp[n].marked)
-      count++;
+      counter++;
     for(int i=0; i<trasp[n].adj.size(); i++){
       int tmp = trasp[n].adj[i];
       dfstrasp(tmp);
     }
+    //trasp[n].found = false;
+    //trasp[n].marked = false;
   }
 }
 
@@ -49,27 +52,24 @@ int main(){
   in >> nodes >> archs;
   graph.resize(nodes);
   trasp.resize(nodes);
+
   for(int i=0; i<archs; i++){
     int index,value;
     in >> index >> value;
     graph[index].adj.push_back(value);
     trasp[value].adj.push_back(index);
-  }
-
-  for(int i=0; i<nodes; i++){
-    graph[i].marked = false;
-    trasp[i].marked = false;
+    trasp[index].marked = false;
+    trasp[value].marked = false;
   }
 
   int max = 0;
   for(int i=0; i<nodes; i++){
     dfs(i);
     dfstrasp(i);
-    if(count > max)
-      max = count;
-    count = 0;
+    if(counter > max)
+      max = counter;
+    counter = 0;
     for(int i=0; i<nodes; i++){
-      graph[i].marked = false;
       trasp[i].marked = false;
       graph[i].found = false;
       trasp[i].found = false;
